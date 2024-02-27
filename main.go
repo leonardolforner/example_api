8217ae2b998646167a4fdf5ae85149dfc2d5ccdb
leonardolforner <leonardoforner@gmail.com>
2024-02-27 05:34:39 -0300

    main updated

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 
-	"github.com/go-chi/chi/v5"
+	"github.com/leonardolforner/example_api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to listen to the server", err)
+		fmt.Println("failed to start app: ", err)
 	}
-}
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!!!"))
 }
